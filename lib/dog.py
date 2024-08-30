5b125d12dd03e3d423abb7588f80e8d8d3b2854a
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-08-30 19:32:59 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print ("Woof!")
+    def sit(self):
+        print ("The dog is sitting.")
+fido =Dog ()
+fido.bark()
 
-    #Instance method definition
-    pass
+snoopy=Dog()
+snoopy.bark ()
+
+class Person:
+    def talk(self):
+        print("Hello, World")
+guido =Person()
+guido.talk()
